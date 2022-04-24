aec3ed8bea00f2300772294ee3f834e9e4968f2a
Ash Hines <ahines@ch-earnest.com>
2022-04-24 18:57:52 +1000

    updates

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -10,6 +10,7 @@ import styles from "../../styles/pokemon.module.css";
 import Axios from "axios";
 
 import { BuildableResource, Properties } from "tapi.js";
+import { Stats } from "fs";
 
 @Properties.Resource
 class PokemonSpecies extends BuildableResource {
@@ -33,27 +34,28 @@ class PokemonGeneration extends BuildableResource {
   }
 }
 
-@Properties.Resource
-class PokemonType extends BuildableResource {
-  public name: string = "";
-  public url: string = "";
+type Pokeman = {
+  id: number;
+  name: string;
+  types: Array<type>;
+  description: string;
+  sprite: string;
+  stats: Array<pokemonStat>;
+};
 
-  // Define a constructor with no arguments.
-  constructor() {
-    super();
-  }
-}
+type type = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
 
-@Properties.Resource
-class PokemonTypesSlot extends BuildableResource {
-  public slot: number = 0;
-  public type: PokemonType = new PokemonType();
-
-  // Define a constructor with no arguments.
-  constructor() {
-    super();
-  }
-}
+type pokemonStat = {
+  base_stat: number;
+  effort: number;
+  stat_name: string;
+};
 
 const getColourFromType = (type: string): string => {
   var result: string;
@@ -113,84 +115,107 @@ const getColourFromType = (type: string): string => {
   return result;
 };
 
-const Pokemon = ({
-  pokemonData,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+function Pokemon({
+  pokemanDetails,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
-
-  const { name } = router.query;
 
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
-  var types: Array<PokemonTypesSlot> = [];
+  var pokemonData: Pokeman = pokemanDetails;
 
-  for (var i = 0; i < pokemonData.types.length; i++) {
-    types.push(new PokemonTypesSlot().fromJSON(pokemonData.types[i]));
-  }
+  console.log(pokemonData.stats);
 
   if (pokemonData.types.length === 1) {
-    var G_PRI = getColourFromType(types[0].type.name);
-    var G_ALT = getColourFromType(types[0].type.name);
+    var G_PRI = getColourFromType(pokemonData.types[0].type.name);
+    var G_ALT = getColourFromType(pokemonData.types[0].type.name);
   } else {
-    var G_PRI = getColourFromType(types[0].type.name);
-    var G_ALT = getColourFromType(types[1].type.name);
+    var G_PRI = getColourFromType(pokemonData.types[0].type.name);
+    var G_ALT = getColourFromType(pokemonData.types[1].type.name);
   }
 
   const sectionStyle = {
     backgroundImage: `linear-gradient(to right, ${G_PRI}, ${G_ALT})`,
   };
 
+  var npid: number = pokemonData.id;
+  var pid = npid.toString();
+  pid = pid.padStart(3, "0");
+
   return (
     <>
-      <Head>
-        <title>PokeDex - {pokemonData.name}</title>
-      </Head>
+      {
+        <Head>
+          <title>{pokemonData.name} | PokeDex</title>
+        </Head>
+      }
       <section
         id="titleSection"
         className={styles.titleSection}
         style={sectionStyle}
       >
-        <h1 className={"capitalize " + styles.h1}>{name}</h1>
-        <Image
-          className={styles.sprite}
-          src={pokemonData.sprites.front_default}
-          alt=""
-          height="96px"
-          width="96px"
-        />
+        <h1 className={"capitalize " + styles.h1}>
+          {pokemonData.name} - #{pid}
+        </h1>
+        {
+          <Image
+            className={styles.sprite}
+            src={pokemonData.sprite}
+            alt=""
+            height="96px"
+            width="96px"
+          />
+        }
+        {<p>{pokemonData.description}</p>}
       </section>
-      <h2>Types</h2>
-      {types.map((value) => {
-        return <p key={value.type.name}>{value.type.name}</p>;
-      })}
+      <h2>Profile</h2>
     </>
   );
-};
+}
+
+// TODO Build Interfaces and reduce to only required details
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  try {
-    const res = await Axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${context.params?.name}/`
-    );
+  const pokemonRequest = await Axios.get(
+    `https://pokeapi.co/api/v2/pokemon/${context.params?.name}/`
+  );
 
-    if (res.status == 404 || !res.data) {
-      return {
-        notFound: true,
-      };
-    }
+  var pokemonID: number = pokemonRequest.data.id;
 
-    const pokemonData = res.data;
+  var types = [];
+  var stats = [];
 
-    return {
-      props: { pokemonData },
-    };
-  } catch (Error) {
-    return {
-      notFound: true,
-    };
+  for (var i = 0; i < pokemonRequest.data.types.length; i++) {
+    types.push(pokemonRequest.data.types[i]);
   }
+
+  for (var i = 0; i < pokemonRequest.data.stats.length; i++) {
+    var stat: pokemonStat = {
+      base_stat: pokemonRequest.data.stats[i].base_stat,
+      effort: pokemonRequest.data.stats[i].effort,
+      stat_name: pokemonRequest.data.stats[i].stat.name,
+    };
+    stats.push(stat);
+  }
+
+  const speciesRequest = await Axios.get(
+    `https://pokeapi.co/api/v2/pokemon-species/${pokemonID}`
+  );
+
+  const pokemanDetails: Pokeman = {
+    id: pokemonID,
+    name: pokemonRequest.data.name,
+    description: speciesRequest.data.flavor_text_entries[0].flavor_text,
+    sprite: pokemonRequest.data.sprites.front_default,
+    types: types,
+    stats: stats,
+  };
+
+  return {
+    props: { pokemanDetails },
+  };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -199,7 +224,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   let pokemonGeneration = new PokemonGeneration().fromJSON(data);
 
   const paths = pokemonGeneration.pokemon_species.map((pokemon_list) => ({
-    params: { name: pokemon_list.name, url: pokemon_list.url },
+    params: { name: pokemon_list.name },
   }));
   return {
     paths,
